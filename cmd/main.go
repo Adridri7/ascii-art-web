fcd7f79328f584bfba6327a2cfb20c7bb940d30f
arocchet <adrieen.rocchetti@gmail.com>
2024-06-11 23:12:47 +0200

    Major update

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"handlers"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -12,7 +13,7 @@ var Text string
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.Error(w, "ERROR: "+strconv.Itoa(http.StatusNotFound)+". PAGE NOT FOUND ", http.StatusNotFound)
+		http.Error(w, "ERROR: "+strconv.Itoa(http.StatusNotFound)+". PAGE NOT FOUND", http.StatusNotFound)
 		return
 	}
 
@@ -26,41 +27,53 @@ func Home(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		text := r.FormValue("input")
-		theme := r.FormValue("themes")
-
-		Text = ascii.CheckArgs([]string{text, theme})
+		text, theme := r.FormValue("input"), r.FormValue("themes")
+		lines := ascii.ThemeToLines(theme)
+		input := ascii.GetTextInput(text)
+		Text = ascii.PrintAsciiArt(input, lines)
+		SaveOutput(Text)
 		http.Redirect(w, r, "/ascii-art", http.StatusSeeOther)
+
 	default:
-		http.Error(w, "ERROR: "+strconv.Itoa(http.StatusMethodNotAllowed)+". METHOD NOT ALLOWED ", http.StatusMethodNotAllowed)
-		fmt.Println("ERROR: " + strconv.Itoa(http.StatusMethodNotAllowed) + ". METHOD NOT ALLOWED ")
+		http.Error(w, "ERROR: "+strconv.Itoa(http.StatusMethodNotAllowed)+". METHOD NOT ALLOWED", http.StatusMethodNotAllowed)
+		fmt.Println("ERROR: " + strconv.Itoa(http.StatusMethodNotAllowed) + ". METHOD NOT ALLOWED")
 	}
 }
 
 func DisplayResult(w http.ResponseWriter, r *http.Request) {
+
 	if r.URL.Path != "/ascii-art" {
-		http.Error(w, "ERROR: "+strconv.Itoa(http.StatusNotFound)+". PAGE NOT FOUND ", http.StatusNotFound)
+		http.Error(w, "ERROR: "+strconv.Itoa(http.StatusNotFound)+". PAGE NOT FOUND", http.StatusNotFound)
 		return
 	}
 
 	switch r.Method {
 	case "GET":
 		handlers.RenderTemplate(w, "ascii-art", Text)
-		fmt.Fprintf(w, Text)
+		//w.Header().Set("Content-Disposition", "attachment; filename=test.txt")
 	default:
-		http.Error(w, "ERROR: "+strconv.Itoa(http.StatusMethodNotAllowed)+". METHOD NOT ALLOWED ", http.StatusMethodNotAllowed)
-		fmt.Println("ERROR: " + strconv.Itoa(http.StatusMethodNotAllowed) + ". METHOD NOT ALLOWED ")
+		http.Error(w, "ERROR: "+strconv.Itoa(http.StatusMethodNotAllowed)+". METHOD NOT ALLOWED", http.StatusMethodNotAllowed)
+		fmt.Println("ERROR: " + strconv.Itoa(http.StatusMethodNotAllowed) + ". METHOD NOT ALLOWED")
 	}
+}
 
-	// Display to the screen
+func SaveOutput(text string) {
+	output := CreateFile("../web/download/res.txt")
+	output.WriteString(text)
+}
 
+func CreateFile(path string) *os.File {
+	file, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	return file
 }
 
 func main() {
 	// Setting up the home page
-
-	fs := http.FileServer(http.Dir("../web/static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../web/static"))))
+	http.Handle("/download/", http.StripPrefix("/download/", http.FileServer(http.Dir("../web/download"))))
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/ascii-art", DisplayResult)
--- pkg/ascii/ascii.go
+++ pkg/ascii/ascii.go
@@ -1,7 +1,6 @@
 package ascii
 
 import (
-	"fmt"
 	"os"
 	"strings"
 )
@@ -15,78 +14,33 @@ func ThemeToLines(s string) []string {
 }
 
 // Get text input from arguments and split it into differente lines.
-func GetTextInput(input string) []string {
-	inputCorrected := strings.Replace(input, "\\n", "\n", -1)
+func GetTextInput(s string) []string {
+	inputCorrected := strings.Replace(s, "\\n", "\n", -1)
+	inputCorrected = strings.Replace(inputCorrected, string(rune(10))+string(rune(13)), "\n", -1)
+	inputCorrected = strings.Replace(inputCorrected, string(rune(13))+string(rune(10)), "\n", -1)
+	inputCorrected = strings.Replace(inputCorrected, "à", "a", -1)
+	inputCorrected = strings.Replace(inputCorrected, "é", "e", -1)
+	inputCorrected = strings.Replace(inputCorrected, "è", "e", -1)
+	inputCorrected = strings.Replace(inputCorrected, "ç", "c", -1)
+	inputCorrected = strings.Replace(inputCorrected, "ù", "u", -1)
 	return strings.Split(inputCorrected, "\n")
 }
 
 // Print the ascii-art.
 func PrintAsciiArt(input, lines []string) string {
-	res := ""
+	s := ""
 	for _, inputLine := range input {
 		if inputLine == "" {
-			res += "\n"
+			s += "\n"
 		} else {
 			for line := 1; line < chunkSize; line++ {
 				for char := 0; char < len(inputLine); char++ {
 					characterStart := (int(inputLine[char]) - 32) * chunkSize
-					res += lines[characterStart+line]
+					s += lines[characterStart+line]
 				}
-				res += "\n"
+				s += "\n"
 			}
 		}
 	}
-	return res
-}
-
-func SaveOutput(text, fileName string) {
-	output := CreateFile(fileName)
-	output.WriteString(text)
-}
-
-func CreateFile(path string) *os.File {
-	file, err := os.Create(path)
-	if err != nil {
-		panic(err)
-	}
-	return file
-}
-
-func CheckArgs(args []string) string {
-	if len(args) <= 1 {
-		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]")
-		os.Exit(0)
-	} else if !ThemeIsValid(args) {
-		fmt.Println("Please provide a valid theme: [shadow/standard/thinkertoy]\nEX: go run . --output=<fileName.txt> something standard")
-		os.Exit(0)
-	} else if len(args) == 3 {
-		if !strings.HasPrefix(args[0], "--output=") {
-			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\nEX: go run . --output=<fileName.txt> something standard")
-			os.Exit(0)
-		} else {
-			fileName := args[0][9:]
-			if !strings.HasSuffix(fileName, ".txt") {
-				fileName += ".txt"
-			}
-			lines := ThemeToLines(args[2])
-			input := GetTextInput(args[1])
-			text := PrintAsciiArt(input, lines)
-			SaveOutput(text, fileName)
-			return text
-		}
-	} else if len(args) == 2 {
-		lines := ThemeToLines(args[1])
-		input := GetTextInput(args[0])
-		return PrintAsciiArt(input, lines)
-	} else {
-		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]")
-		os.Exit(0)
-	}
-	return ""
-}
-
-func ThemeIsValid(args []string) bool {
-	return args[len(args)-1] == "shadow" ||
-		args[len(args)-1] == "standard" ||
-		args[len(args)-1] == "thinkertoy"
+	return s
 }
